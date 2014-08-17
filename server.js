35734dfe8f1389702f4ef940fb284cf1833a6cbf
bhailogee <muhammad.wasu@gmail.com>
2014-08-17 19:25:49 +0500

    change1

--- server.js
+++ server.js
@@ -52,7 +52,8 @@ app.use(express.static((config.dirname || ".") + "/public"));
 app.listen(port, host);
 
 var fs = require("fs");
-console.log("opening");
-
+console.log("Server running at pot: " + port);
+console.log("Go to URL http://localhost:" + port + "/index.html for demo purpose");
+Console.log("You can go to config.json to specify log_file");
 var bytesRead = 0;
 
